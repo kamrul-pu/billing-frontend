f3fc4f0501f6d8604d3b06c8e85297411b87eedc
Kamrul Hasan <kamrul@evidentbd.com>
2025-08-09 13:08:21 +0600

    dockerize with nginx

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Allow common local network IPs and patterns
-  allowedDevOrigins: [
-    'localhost:3000',
-    '127.0.0.1:3000',
-    '0.0.0.0:3000',
-
-    // Match any device on common local subnets
-    '192.168.0.*:3000',
-    '192.168.1.*:3000',
-    '192.168.68.*:3000',
-    '192.168.100.*:3000',
-
-    // For 10.x.x.x networks (common in corporate/advanced setups)
-    '10.*.*.*:3000',
-
-    // If you use custom domains via /etc/hosts
-    'local-origin.dev',
-    '*.local-origin.dev',
-  ],
+  allowedDevOrigins: ['*'],
+  // basePath: '/frontend', // to use as localhost/frontend
 };
 
 export default nextConfig;
